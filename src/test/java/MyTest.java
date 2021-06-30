62ed429da3a1552db1262dd44995279ac71c9935
P.Mirzapour <pooyamirzapour@yahoo.com>
2021-06-30 13:11:02 +0430

    init

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+    @Test
+    void test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test3() {
+        assertTrue(true);
+    }
+}
